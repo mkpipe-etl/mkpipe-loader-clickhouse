7bb5ba2b568c0f5c1a2d5d4a317d72b87c76146e
m-karakus <s.metinkarakus@gmail.com>
2026-03-30 23:27:09 +0300

    jar version update

--- mkpipe_loader_clickhouse/__init__.py
+++ mkpipe_loader_clickhouse/__init__.py
@@ -1,13 +1,13 @@
 import gc
 from datetime import datetime
 
+from mkpipe.models import ConnectionConfig, ExtractResult, TableConfig
 from mkpipe.spark.base import BaseLoader
 from mkpipe.spark.columns import add_etl_columns
-from mkpipe.models import ConnectionConfig, ExtractResult, TableConfig
 from mkpipe.utils import get_logger
 
 JAR_PACKAGES = [
-    'com.clickhouse.spark:clickhouse-spark-runtime-4.0_2.13:0.8.1',
+    'com.clickhouse.spark:clickhouse-spark-runtime-4.0_2.13:0.10.0',
     'com.clickhouse:clickhouse-http-client:0.7.2',
     'org.apache.httpcomponents.client5:httpclient5:5.3.1',
 ]
@@ -39,7 +39,9 @@ class ClickhouseLoader(BaseLoader, variant='clickhouse'):
         df = data.df
 
         if df is None:
-            logger.info({'table': target_name, 'status': 'skipped', 'reason': 'no data'})
+            logger.info(
+                {'table': target_name, 'status': 'skipped', 'reason': 'no data'}
+            )
             return
 
         df = add_etl_columns(df, datetime.now(), dedup_columns=table.dedup_columns)
@@ -47,7 +49,9 @@ class ClickhouseLoader(BaseLoader, variant='clickhouse'):
         if table.write_partitions:
             df = df.coalesce(table.write_partitions)
 
-        logger.info({'table': target_name, 'status': 'loading', 'write_mode': write_mode})
+        logger.info(
+            {'table': target_name, 'status': 'loading', 'write_mode': write_mode}
+        )
 
         opts = {**self._base_options(), 'dbtable': target_name}
         writer = df.write.format('clickhouse').mode(write_mode)
